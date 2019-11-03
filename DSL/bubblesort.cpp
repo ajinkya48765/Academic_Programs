0884de6a30945cbf4a3e333e2398eca45444b69c
Ajinkya S Khandave <52904749+ajinkya48765@users.noreply.github.com>
2019-11-03 11:06:42 +0530

    Create bubblesort.cpp

---
+++ DSL/bubblesort.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a[10];
+	int n;
+	int pass, temp;
+	cout<<"Enter number of elements of array  :  ";
+	cin>>n;
+
+	cout<<"Enter elements of an array  :  ";
+	for (int i = 0; i < n; i++)
+	{
+		cin>>a[i];
+	}
+
+	//BUBBLE SORT
+	for (int i = 1; i < n; i++)
+	{
+		for (int j = 0; j < n-i ; j++)
+		{
+			if(a[j]>a[j+1])
+			{
+				temp=a[j];
+				a[j]=a[j+1];
+				a[j+1]=temp;
+			}
+		}
+	}
+
+
+	for (int i = 0; i < n; i++)
+	{
+		cout<<a[i]<<" ";
+	}
+}
